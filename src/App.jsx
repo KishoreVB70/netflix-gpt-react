d19d2139e1e0793164585071a1531fd722396e2b
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-08 15:07:42 +0530

    added react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import 
+
+function App() {
+  return (
+    <div className="w-screen h-screen ">
+      <h1 className='text-blue-500'>Netflix GPT in the house perro</h1>
+    </div>
+  );
+}
+
+export default App;
