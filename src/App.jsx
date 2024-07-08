1e86fae912efd1334d2d9174ff0b417b84592aea
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-08 15:24:19 +0530

    Added browser routing using router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import './App.css';
-import 
+import { createBrowserRouter, BrowserRouter, Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Demo from './components/Demo';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className="w-screen h-screen ">
-      <h1 className='text-blue-500'>Netflix GPT in the house perro</h1>
+      <Header />
+      <Outlet />
     </div>
   );
 }
+
+export const browserRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/demo",
+        element: <Demo />
+      }
+    ]
+  }
+])
+
+
+
 
 export default App;
