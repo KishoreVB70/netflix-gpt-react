95c67a4faa63993fff1905aca2e43b165423fc9c
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-09 20:58:11 +0530

    added movies to slice

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react'
 import { tmdbNowStreamingApi, tmdbOptions } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../redux/movies';
 
 const Body = () => {
+
+  const dispatch = useDispatch();
 
   const fetchStreamingMovies  = async() => {
     try {
       const _data = await fetch(tmdbNowStreamingApi, tmdbOptions)
       const data = await _data.json();
       console.log(data);
+      dispatch(addMovies(data.results));
     }catch (error){
       console.log(error);
     }
