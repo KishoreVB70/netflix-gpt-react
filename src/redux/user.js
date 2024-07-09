2629d48af8080fdedcd5c524dfdf2119997a1df2
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-09 14:19:43 +0530

    added remove method to the slice

--- src/redux/user.js
+++ src/redux/user.js
@@ -2,24 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const user = createSlice({
     name: "user",
-    initialState: {
-        // uid: null,
-        // displayName: null,
-        // email: null,
-        // emailVerified: false
-    },
+
+    initialState: null,
     
     reducers: {
         setUser: (state, action) => {
             return {
                 ...action.payload
             }
-            // const {uid, email, emailVerified, displayName} = action.payload;
-            // state.uid = uid;
-            // state.email = email;
-            // state.emailVerified = emailVerified; 
-            // state.displayName = displayName;
-        }
+        },
+
+        removeUser: () => null
     }
 })
 
