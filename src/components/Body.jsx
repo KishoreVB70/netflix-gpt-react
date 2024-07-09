a661fb60cacd31c55203ab4ffd20a51148e72027
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-09 21:05:08 +0530

    created custom hook for fetching streaming movies

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,27 +1,8 @@
 import React, { useEffect } from 'react'
-import { tmdbNowStreamingApi, tmdbOptions } from '../utils/constants';
-import { useDispatch } from 'react-redux';
-import { addMovies } from '../redux/movies';
-
+import useGetStreamingMovies from '../utils/useGetStreamingMovies'
 const Body = () => {
-
-  const dispatch = useDispatch();
-
-  const fetchStreamingMovies  = async() => {
-    try {
-      const _data = await fetch(tmdbNowStreamingApi, tmdbOptions)
-      const data = await _data.json();
-      console.log(data);
-      dispatch(addMovies(data.results));
-    }catch (error){
-      console.log(error);
-    }
-  }
-
- useEffect(() => {
-  fetchStreamingMovies();
- },[])
-
+  useGetStreamingMovies();
+  
   return (
     <div>Body</div>
   )
---
+++ src/utils/useGetStreamingMovies.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import { tmdbNowStreamingApi, tmdbOptions } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../redux/movies';
+
+const useGetStreamingMovies = () => {
+    const dispatch = useDispatch();
+    
+    const fetchStreamingMovies  = async() => {
+      try {
+        const _data = await fetch(tmdbNowStreamingApi, tmdbOptions)
+        const data = await _data.json();
+        console.log(data);
+        dispatch(addMovies(data.results));
+      }catch (error){
+        console.log(error);
+      }
+    }
+    
+    useEffect(() => {
+        fetchStreamingMovies();
+    },[])
+}
+
+export default useGetStreamingMovies;
