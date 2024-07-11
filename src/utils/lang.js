8400b1707abc6ae2732d2c12f575ff88c7bfbe0e
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-11 12:58:23 +0530

    added language constants

---
+++ src/utils/lang.js
@@ -0,0 +1,18 @@
+export const lang = {
+    en: {
+        search: 'Search',
+        searchPlaceHolder:'What do you feel like watching' 
+    },
+    
+    tn: {
+        search: 'தேடு',
+        searchPlaceHolder: 'நீங்கள் எதைப் பார்க்க விரும்புகிறீர்கள்?'
+    
+    },
+    
+    hn: {
+        search: 'खोज',
+        searchPlaceHolder: 'आपको क्या देखने का मन कर रहा है?' 
+    
+    },
+}
