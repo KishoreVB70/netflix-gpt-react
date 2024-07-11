1c20d5291d6d45a770f929ffc281abc233b91083
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-11 12:59:54 +0530

    imported langugae to gpt page

--- src/components/GPTSearch.jsx
+++ src/components/GPTSearch.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react'
+import { lang } from '../utils/lang';
 
 const GPTSearch = () => {
     const GPTSearchInput = useRef(null);
@@ -8,8 +9,8 @@ const GPTSearch = () => {
 
     return (
         <div className='bg-black w-1/2 h-[7%] flex flex-row items-center justify-center'>
-            <input ref={GPTSearchInput} className='text-xl h-full w-3/4 p-2 pl-4 border border-black' placeholder='What do you feel like watching' type="text" />
-            <button onClick={handleGPTSearch} className='bg-red-600 w-1/4 h-full border border-black' > Search </button>
+            <input ref={GPTSearchInput} className='text-xl h-full w-3/4 p-2 pl-4 border border-black' placeholder={lang.tn.searchPlaceHolder} type="text" />
+            <button onClick={handleGPTSearch} className='bg-red-600 w-1/4 h-full border border-black text-xl' > {lang.tn.search} </button>
         </div>  
     )
 }
