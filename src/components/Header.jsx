5af3b3dd2cb6c1fc97e369bd26637d3073b9d9f9
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-12 07:23:24 +0530

    added different styling for different devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
   }
 
   return (
-    <div className='flex flex-row w-screen h-[10%] bg-transparent items-center mb-2 p-2 justify-between '>
+    <div className='md:bg-red-100 flex flex-row w-screen h-[10%] bg-transparent items-center mb-2 p-2 justify-between '>
       <div className='ml-10 h-full w-1/6' >
         <img className='h-full w-1/2  transform scale-150 ' src={netflixLogoURL} alt="netflix-logo" />
       </div>
