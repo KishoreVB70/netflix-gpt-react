cdaed10bcf9f947b2cdca35c96494b7f2d1cbf2c
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-19 12:46:42 +0530

    margin changes to movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard'
 
 const MovieList = ({movies, title}) => {
   return (
-    <div className='flex flex-col items-center m-2' >
+    <div className='flex flex-col items-center m-2 mb-10' >
         <h1 className='text-2xl font-bold text-white' >{title}</h1>
         <div className='flex flex-row flex-wrap justify-start mt-5' >
           {movies.map(i => <MovieCard key={i.id} movie={i} />)}
