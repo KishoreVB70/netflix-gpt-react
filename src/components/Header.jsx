8b9ea8577526da0d8a54a3f6bbb95938b22d023d
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-19 13:19:44 +0530

    made the header workable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,21 +63,20 @@ const Header = () => {
   }
 
   return (
-    <div className=' flex flex-row w-screen h-[20%] md:h-[10%] bg-transparent items-center mb-2 p-2 justify-between '>
-      <div className='ml-10 h-full w-1/6' >
-      <Link to={"/browse"}>
-        <img className='hover:cursor-pointer h-full w-1/2 transform scale-150 ' src={netflixLogoURL} alt="netflix-logo" />
-      </Link>
+    <div className=' flex flex-col md:flex-row md:h-[10%] bg-transparent items-center md:mb-2 p-2 md:justify-between'>
+      <div className='md:ml-10 md:h-full md:w-1/6 flex justify-center md:justify-start' >
+        <Link className='w-4/12 md:w-1/2' to={"/browse"}>
+          <img className='hover:cursor-pointer md:h-full md:w-full transform scale-150' src={netflixLogoURL} alt="netflix-logo" />
+        </Link>
       </div>
-      <div className='flex flex-row w-2/6 h-full items-center mr-8 justify-end' >
-      {isGptPage &&
-        <select className=' bg-transparent w-24 border border-white text-white hover:cursor-pointer p-2' onChange={changeLanguage}>
-          <option className='text-black ' value="en">English</option>
-          <option className='text-black ' value="tn">Tamil</option>
-          <option className='text-black ' value="hn">Hindi</option>
-        </select>
-      }
-        <img className='h-[85%]' src={user.photoURL} alt="user-icon object-cover" />
+      <div className='flex flex-row md:w-2/6 md:h-full items-center md:mr-8 md:justify-end justify-between' >
+        {isGptPage &&
+          <select className='hidden md:block bg-transparent w-24 border border-white text-white hover:cursor-pointer p-2' onChange={changeLanguage}>
+            <option className='text-black ' value="en">English</option>
+            <option className='text-black ' value="tn">Tamil</option>
+            <option className='text-black ' value="hn">Hindi</option>
+          </select>}
+        <img className='hidden md:block h-[85%]' src={user.photoURL} alt="user-icon object-cover" />
         <h1 className='text-white text-xl font-semibold' >{user.displayName}</h1>
         <button onClick={handleActionButtonClick} className='transition duration-300 ease-in-out hover:rounded-xl text-md flex justify-center items-center txt-center w-24 h-1/2 p-2 text-white mx-2 border-red-600 hover:bg-red-600 border' >{actionButtonName}</button>
         <button onClick={handleSignOut} className='text-md flex justify-center items-center txt-center w-24 h-1/2 p-2 text-white border-white border ' >Sign Out</button>
