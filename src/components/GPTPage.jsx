19d478fa6a12fc66083a620e543330508dbb766e
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-19 07:00:03 +0530

    moved selector to the gpt search element

--- src/components/GPTPage.jsx
+++ src/components/GPTPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import GPTSearch from './GPTSearch'
 import { netflixLoginBackgroundImageURL } from '../utils/constants'
+import { useSelector } from 'react-redux';
+
 
 const GPTPage = () => {
+  const gptMovies = useSelector(s => s.movies.gptMovies);
+
   return (
-    <div className='w-screen h-[70%] flex items-center justify-center'>
-      <div className='absolute inset-0 bg-black h-screen w-screen -z-40 opacity-20' ></div>
-      <img className='absolute inset-0 w-screen h-screen -z-50' src={netflixLoginBackgroundImageURL} alt="netflix-background-image" />
-      <GPTSearch />
+    <div className='h-[85%] w-full flex flex-col'>
+      <div className='w-screen h-[70%] flex items-center justify-center'>
+        <div className='absolute inset-0 bg-black h-screen w-screen -z-40 opacity-20' ></div>
+        <img className='absolute inset-0 w-screen h-screen -z-50' src={netflixLoginBackgroundImageURL} alt="netflix-background-image" />
+        <GPTSearch />
+      </div>
+      {gptMovies && 
+            gptMovies.map(i => <h1 className='text-white' key={i.id}>{i.title}</h1>)
+      }
     </div>
   )
 }
--- src/components/GPTSearch.jsx
+++ src/components/GPTSearch.jsx
@@ -11,7 +11,6 @@ const GPTSearch = () => {
     const language = useSelector(s => s.config.lang);
     const REACT_APP_OPEN_AI_API_KEY = process.env.REACT_APP_OPEN_AI_API_KEY;
     const dispatch = useDispatch();
-    const gptMovies = useSelector(s => s.movies.gptMovies);
 
     const openai = new OpenAI({apiKey: REACT_APP_OPEN_AI_API_KEY,
           dangerouslyAllowBrowser: true        });
@@ -47,12 +46,10 @@ const GPTSearch = () => {
             // console.log(oneDArray);
             
             // Remove undefined
-            const finalArray = oneDArray.filter(i => i);
+            const finalArray = oneDArray.filter(i => i?.backdrop_path);
             console.log(finalArray);
 
             dispatch(addGptMovies(finalArray));
-
-
 
         }catch(error) {
             console.log(error);
@@ -60,15 +57,10 @@ const GPTSearch = () => {
     }
 
     return (
-        <>
         <div className='bg-black w-1/2 h-[7%] flex flex-row items-center justify-center'>
             <input ref={GPTSearchInput} className='text-xl h-full w-3/4 p-2 pl-4 border border-black' placeholder={lang[language].searchPlaceHolder} type="text" />
             <button onClick={handleGPTSearch} className='bg-red-600 w-1/4 h-full border-y border-r text-white border-black text-xl' > {lang[language].search} </button>
         </div>
-        {gptMovies && 
-            gptMovies.map(i => <h1 className='text-white' key={i.id}>{i.title}</h1>)
-        }
-        </>
     )
 }
 
