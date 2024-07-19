fd8344bd315b90933e38d57af4874373cc824d56
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-19 11:54:50 +0530

    added loading shimmer for poster

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { tmdbPosterBaseUrl } from '../utils/constants';
 
 const MovieCard = ({movie}) => {
+  const [loaded, setLoaded] = useState(false);
   const navigate = useNavigate();
   return (
     <div className='my-2 mx-2 w-[10vw]'>
-      <img onClick={() => navigate("/browse/movie/"+movie.id)} className='h-full w-full hover:cursor-pointer hover:-translate-y-3 hover:shadow-lg transition-all duration-300' src={`${tmdbPosterBaseUrl}${movie.poster_path}`} alt="movie-image" />
+      {!loaded && 
+        <div className='w-full h-full bg-gray-100'></div>
+      }
+      <img onLoad={() => setLoaded(true)} onClick={() => navigate("/browse/movie/"+movie.id)} className={` ${loaded ? "block" : "hidden"} w-full hover:cursor-pointer hover:-translate-y-3 hover:shadow-lg transition-all duration-300`} src={`${tmdbPosterBaseUrl}${movie.poster_path}`} alt="movie-image" />
     </div>
   )
 }
