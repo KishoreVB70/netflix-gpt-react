f04f392739baa95a55a172c84b662d4f33cf374b
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-19 14:26:29 +0530

    moved firebase login to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,20 @@
+//Assets
 export const netflixLoginBackgroundImageURL = "https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/537e2c5e-c750-4d4c-9f7a-e66fe93eb977/IN-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_b00eeb83-a7e8-4b5b-8ff7-86ed92c51caf_large.jpg" 
 
 export const neflixUserIconURL = "https://www.iconpacks.net/icons/5/free-icon-gamer-boy-15169.png"
 
 export const netflixLogoURL = "https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png"
 
+// firebase
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "gpt-netflix-7.firebaseapp.com",
+  projectId: "gpt-netflix-7",
+  storageBucket: "gpt-netflix-7.appspot.com",
+  messagingSenderId: "822961500160",
+  appId: "1:822961500160:web:a541f7ca8d4233db0da8e5",
+  measurementId: "G-LDHSN0FDQ4"
+};
 // tmdb
 export const trailerNumber = 0;
 
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,20 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { firebaseConfig } from "./constants";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBKFO86cemBowR08GnL8ptmEYLjwCe6FEM",
-  authDomain: "gpt-netflix-7.firebaseapp.com",
-  projectId: "gpt-netflix-7",
-  storageBucket: "gpt-netflix-7.appspot.com",
-  messagingSenderId: "822961500160",
-  appId: "1:822961500160:web:a541f7ca8d4233db0da8e5",
-  measurementId: "G-LDHSN0FDQ4"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
